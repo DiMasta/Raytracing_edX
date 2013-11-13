f857308c57e20bf352e81589d6828918498c2785
Vladislav Vulchev <vladislav.vulchev@chaosgroup.com>
2013-11-13 11:14:34 +0200

    Move back to SDL 1.2 for more the lack of tutorials for 2.0 ;)

--- DiTracer/DiTracer/main.cpp
+++ DiTracer/DiTracer/main.cpp
@@ -1,52 +1,38 @@
-#include <iostream>
-#if defined(_MSC_VER)
-#include <SDL.h>
-#else
-#include <SDL2/SDL.h>
-#endif
+#include "SDL.h"
 
-/**
-* Lesson 1: Hello World!
-*/
-int main(int argc, char** argv){
-        //First we need to start up SDL, and make sure it went ok
-        if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-                std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-                return 1;
-        }
-        
-        //Now create a window with title "Hello World" at 100, 100 on the screen with w:640 h:480 and show it
-        SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-        //Make sure creating our window went ok
-        if (win == nullptr){
-                std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-                return 1;
-        }
+int main( int argc, char* args[] )
+{
+    //Start SDL
+    SDL_Init( SDL_INIT_EVERYTHING );
+    
+	SDL_Surface* displaysurface = SDL_SetVideoMode(
+        640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
 
-        //Create a renderer that will draw to the window, -1 specifies that we want to load whichever 
-        //video driver supports the flags we're passing
-        //Flags: SDL_RENDERER_ACCELERATED: We want to use hardware accelerated rendering
-        //SDL_RENDERER_PRESENTVSYNC: We want the renderer's present function (update screen) to be
-        //synchornized with the monitor's refresh rate
-        SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-        if (ren == nullptr){
-                std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-                return 1;
-        }
-        
-        //Now lets draw our image
-        //First clear the renderer
-        SDL_RenderClear(ren);
-        //Update the screen
-        SDL_RenderPresent(ren);
+	unsigned backgroundcolor = SDL_MapRGB(displaysurface->format, 0, 64, 0);
 
-        //Have the program wait for 2000ms so we get a chance to see the screen
-        SDL_Delay(2000);
+    SDL_Event sdlevent;
+    bool running = true;
 
-        //Clean up our objects and quit
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        SDL_Quit();
-        
-        return 0;
+    while(running)
+    {
+        while(SDL_PollEvent(&sdlevent))
+        {
+            switch(sdlevent.type)
+            {
+            case SDL_QUIT:
+                {
+                    running = false;
+                }break;
+			}
+		}
+
+		SDL_FillRect(displaysurface, NULL, backgroundcolor);
+		SDL_Flip(displaysurface);
+		//SDL_Delay(2000);
+	}
+
+    //Quit SDL
+    SDL_Quit();
+    
+    return 0;    
 }
